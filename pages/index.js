bc457c1d9451deeceb7feeff76f687574f8b89ab
iEasyJet <86961006+iEasyJet@users.noreply.github.com>
2021-09-28 22:44:35 +0300

    Исправления1

--- pages/index.js
+++ pages/index.js
@@ -5,51 +5,61 @@ let editBtn = profile.querySelector('.profile__edit-btn');
 // Находим popup
 let popup = document.querySelector('.popup');
 // Находим кнопку закрытия popup
-let popupSkipBtn = document.querySelector('.popup__img');
+let popupSkipBtn = document.querySelector('.popup__close');
+
+
+
+
+
+  // Функция для копирования значения в value popup
+function popupValue() {
+    // Значение поля с имененем
+  let profileName = document.querySelector('.profile__name-user').innerHTML;
+    // Значение поля с работой
+  let profileJob = document.querySelector('.profile__name-job').innerHTML;
+
+    // Прописываем значения в value input
+  popup.querySelector('.popup__input_type_name').value = profileName;
+  popup.querySelector('.popup__input_type_job').value = profileJob;
+  };
+
 
 // Добавляем новый класс с display block для отображения popup
-function popupOpenned() {
+function openPopup() {
   popup.classList.add('popup_opened');
 };
 
 // Удаляем новый класс с display block для скрытия popup
-function popupSkip() {
+function closePopup() {
   popup.classList.remove('popup_opened');
 };
-
-
-
-
-
-
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__input-name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__input-job');// Воспользуйтесь инструментом .querySelector()
+let nameInput = formElement.querySelector('.popup__input_type_name');// Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__input_type_job');// Воспользуйтесь инструментом .querySelector()
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    // Получите значение полей jobInput и nameInput из свойства value
-    let nameInputValue = nameInput.value;
-    let jobInputValue = jobInput.value;
     // Выберите элементы, куда должны быть вставлены значения полей
     let userName = profile.querySelector('.profile__name-user');
     let userJob = profile.querySelector('.profile__name-job');
     // Вставьте новые значения с помощью textContent
-    userName.textContent = nameInputValue;
-    userJob.textContent = jobInputValue;
-}
+    userName.textContent = nameInput.value;
+    userJob.textContent = jobInput.value;
+};
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', popupSkip);
+formElement.addEventListener('submit', closePopup);
 //Ставим прослушку на кнопку редактирования, чтобы открыть popup
-editBtn.addEventListener('click', popupOpenned);
+editBtn.addEventListener('click', openPopup);
+// Прописываем новые значение value в input
+popupValue()
 //Ставим прослушку на кнопку закрытия popup
-popupSkipBtn.addEventListener('click', popupSkip);
+popupSkipBtn.addEventListener('click', closePopup);
