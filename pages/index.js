1c8912c8e130b804d07191f4ee7f7f5ea6139c69
iEasyJet <86961006+iEasyJet@users.noreply.github.com>
2021-09-29 19:59:12 +0300

    fix: исправления#2

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,21 @@
 let profile = document.querySelector('.profile');
 // Находим кнопку редактирования
 let editBtn = profile.querySelector('.profile__edit-btn');
+// Значение поля с имененем profile
+let profileName = profile.querySelector('.profile__name-user');
+// Значение поля с работой profile
+let profileJob = profile.querySelector('.profile__name-job');
+
 // Находим popup
 let popup = document.querySelector('.popup');
 // Находим кнопку закрытия popup
-let popupSkipBtn = document.querySelector('.popup__close');
+let popupSkipBtn = popup.querySelector('.popup__close');
+// Находим форму в DOM
+let formElement = popup.querySelector('.popup__form');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_job');
 
-
-
-
-
-  // Функция для копирования значения в value popup
-function popupValue() {
-    // Значение поля с имененем
-  let profileName = document.querySelector('.profile__name-user').innerHTML;
-    // Значение поля с работой
-  let profileJob = document.querySelector('.profile__name-job').innerHTML;
-
-    // Прописываем значения в value input
-  popup.querySelector('.popup__input_type_name').value = profileName;
-  popup.querySelector('.popup__input_type_job').value = profileJob;
-  };
 
 
 // Добавляем новый класс с display block для отображения popup
@@ -34,32 +29,32 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 };
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');// Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__input_type_name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__input_type_job');// Воспользуйтесь инструментом .querySelector()
+// Функция для копирования значения в value popup
+function popupValue() {
+  // Прописываем значения в value input
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+
+  openPopup()
+  };
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let userName = profile.querySelector('.profile__name-user');
-    let userJob = profile.querySelector('.profile__name-job');
-    // Вставьте новые значения с помощью textContent
-    userName.textContent = nameInput.value;
-    userJob.textContent = jobInput.value;
+    // Вставьте новые значения в разметку
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    closePopup()
 };
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', closePopup);
+
 //Ставим прослушку на кнопку редактирования, чтобы открыть popup
-editBtn.addEventListener('click', openPopup);
-// Прописываем новые значение value в input
-popupValue()
+editBtn.addEventListener('click', popupValue);
+
 //Ставим прослушку на кнопку закрытия popup
 popupSkipBtn.addEventListener('click', closePopup);
